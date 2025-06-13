e191ccc48e598e29d0988cf7b9c2bcb8769dbc84
Sheik1419 <sheikgamer567@gmail.com>
2025-06-13 12:30:44 +0530

    changes

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -7,7 +7,7 @@ const MainLayout = () => {
     return (
         <>
             <Navbar />
-            <main className="min-h-screen p-4">
+            <main>
                 <Outlet />
             </main>
             <Footer />
